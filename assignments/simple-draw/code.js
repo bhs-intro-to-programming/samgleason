4545a5c433d4a65d197fbfd6a2b7b23df83e7133
samgleason <111467179+samgleason@users.noreply.github.com>
2022-08-19 15:13:10 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -39,7 +39,7 @@ drawLine(0, 0, width, height, 'red')
 
 drawCircle(width/2, height/2, 10, 'blue');
 
-drawRect(-15, 30, 40, 30, 'black');
+drawRect(-15, 10, 40, 30, 'black');
 
 drawFilledCircle(width - 50, height - 50, 50, 'red');
 
