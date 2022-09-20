7eafee04c6ae5c4b31f4c24eeb7821d21c58f4a5
samgleason <111467179+samgleason@users.noreply.github.com>
2022-09-19 17:40:14 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,5 +1,5 @@
-let x = myFunction(4, 3);   // Function is called, return value will end up in x
+let x = myFunction(4, 3); 
 
 function myFunction(a, b) {
-  return a * b;             // Function returns the product of a and b
+  return a * b;             
 }
